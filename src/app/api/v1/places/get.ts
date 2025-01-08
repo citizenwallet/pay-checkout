07971c89724a8e5564f315cf7722861e100dc506
sajee_techi <sajeewa@techi.be>
2025-01-08 20:25:19 +0530

    endpoint to get places

---
+++ src/app/api/v1/places/get.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from "next/server";
+import { getServiceRoleClient } from "@/db";
+import { getAllPlaces } from "@/db/places";
+
+/**
+ * GET /api/v1/places
+ *
+ * Returns all places.
+ *
+ * @returns {Object} Response
+ * @returns {Object} Response.places - The places data if found
+ * @returns {string} Response.error - Error message if request fails
+ * @returns {number} Response.status - HTTP status code if request fails
+ *
+ * @example
+ * // Request
+ * GET /api/v1/places
+ *
+ * // Success Response
+ * {
+ *   places: [
+ *     {
+ *         "id": 29,
+ *         "name": "VIDYA. Kanal",
+ *         "slug": "vidya-kanal-tcen",
+ *         "image": null,
+ *         "accounts": "0xbB3676bc08fdD281D7b922A7676e0ee9f2667C8A",
+ *         "description": null
+ *     }
+ *   ]
+ * }
+ *
+ * // Error Response
+ * {
+ *   error: "Failed to get places",
+ *   status: 400
+ * }
+ */
+
+export interface PlaceResponse {
+  id: number;
+  name: string;
+  slug: string;
+  account: string;
+  description: string | null;
+  image: string | null;
+}
+
+export async function GET() {
+  try {
+    const client = getServiceRoleClient();
+    const places = await getAllPlaces(client);
+
+    return NextResponse.json({ places }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to get places" },
+      { status: 500 }
+    );
+  }
+}
