f3abfa31bc39c9c9be354c447677adfba200466b
kev-techi <kevin@techi.be>
2025-02-15 22:30:29 +0100

    wait for order to be created

--- src/app/api/v1/webhooks/viva/transactionPriceCalculated.ts
+++ src/app/api/v1/webhooks/viva/transactionPriceCalculated.ts
@@ -9,6 +9,9 @@ export const transactionPriceCalculated = async (
 
   console.log("TransactionId", TransactionId);
 
+  // Wait for 1 second to ensure the order is created
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const client = getServiceRoleClient();
   const { data: orders, error: orderError } =
     await getTerminalOrderByTransactionId(client, TransactionId);
