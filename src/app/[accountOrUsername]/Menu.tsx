0a6cd1b441fb2dbb265f25aa599f6b60b337c91a
kev-techi <kevin@techi.be>
2025-03-03 22:36:02 +0100

    switch to custom amount based on display property

--- src/app/[accountOrUsername]/Menu.tsx
+++ src/app/[accountOrUsername]/Menu.tsx
@@ -511,86 +511,89 @@ export default function Menu({
                 </div>
               </div>
             )}
-            {Object.entries(itemsByCategory).map(
-              ([category, categoryItems]) => (
-                <div
-                  key={category}
-                  id={`category-${category}`}
-                  ref={(el) => {
-                    if (el) categoryRefs.current[category] = el;
-                  }}
-                >
-                  <h2 className="text-xl font-semibold mb-4">{category}</h2>
-                  {categoryItems.map((item) => (
-                    <Card
-                      key={item.id}
-                      className={`${
-                        selectedItems[item.id] ? "border-primary" : ""
-                      } mb-4`}
-                    >
-                      <CardContent className="flex justify-start items-center gap-2 p-2">
-                        {item.image && (
-                          <div className="pr-2 border-r">
-                            <Image
-                              src={item.image}
-                              alt={item.name}
-                              width={80}
-                              height={80}
-                              className="rounded-md w-20 h-20 object-cover cursor-pointer"
-                              onClick={() => setSelectedImage(item.image!)}
-                            />
+            {!noItems &&
+              Object.entries(itemsByCategory).map(
+                ([category, categoryItems]) => (
+                  <div
+                    key={category}
+                    id={`category-${category}`}
+                    ref={(el) => {
+                      if (el) categoryRefs.current[category] = el;
+                    }}
+                  >
+                    <h2 className="text-xl font-semibold mb-4">{category}</h2>
+                    {categoryItems.map((item) => (
+                      <Card
+                        key={item.id}
+                        className={`${
+                          selectedItems[item.id] ? "border-primary" : ""
+                        } mb-4`}
+                      >
+                        <CardContent className="flex justify-start items-center gap-2 p-2">
+                          {item.image && (
+                            <div className="pr-2 border-r">
+                              <Image
+                                src={item.image}
+                                alt={item.name}
+                                width={80}
+                                height={80}
+                                className="rounded-md w-20 h-20 object-cover cursor-pointer"
+                                onClick={() => setSelectedImage(item.image!)}
+                              />
+                            </div>
+                          )}
+                          <div className="flex flex-1 flex-col justify-center items-start gap-2 min-h-20">
+                            <p className="text-lg font-bold">{item.name}</p>
+                            <p className="text-sm">{item.description}</p>
                           </div>
-                        )}
-                        <div className="flex flex-1 flex-col justify-center items-start gap-2 min-h-20">
-                          <p className="text-lg font-bold">{item.name}</p>
-                          <p className="text-sm">{item.description}</p>
-                        </div>
-                        <div className="flex flex-col justify-between items-end gap-2 h-20">
-                          <div className="flex items-center gap-2">
-                            <CurrencyLogo logo={currencyLogo} size={24} />
-                            <p className="text-lg font-bold">
-                              {formatCurrencyNumber(item.price)}
-                            </p>
-                          </div>
-                          <div className="flex items-center gap-2">
-                            {!selectedItems[item.id] ? (
-                              <Button
-                                variant="secondary"
-                                onClick={() => adjustItemQuantity(item.id, 1)}
-                              >
-                                <PlusIcon className="h-4 w-4" /> Add to Cart
-                              </Button>
-                            ) : (
-                              <div className="flex items-center gap-2">
+                          <div className="flex flex-col justify-between items-end gap-2 h-20">
+                            <div className="flex items-center gap-2">
+                              <CurrencyLogo logo={currencyLogo} size={24} />
+                              <p className="text-lg font-bold">
+                                {formatCurrencyNumber(item.price)}
+                              </p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                              {!selectedItems[item.id] ? (
                                 <Button
-                                  variant="outline"
-                                  size="icon"
-                                  onClick={() =>
-                                    adjustItemQuantity(item.id, -1)
-                                  }
-                                >
-                                  -
-                                </Button>
-                                <span className="min-w-[2rem] text-center">
-                                  {selectedItems[item.id]}
-                                </span>
-                                <Button
-                                  variant="outline"
-                                  size="icon"
+                                  variant="secondary"
                                   onClick={() => adjustItemQuantity(item.id, 1)}
                                 >
-                                  +
+                                  <PlusIcon className="h-4 w-4" /> Add to Cart
                                 </Button>
-                              </div>
-                            )}
+                              ) : (
+                                <div className="flex items-center gap-2">
+                                  <Button
+                                    variant="outline"
+                                    size="icon"
+                                    onClick={() =>
+                                      adjustItemQuantity(item.id, -1)
+                                    }
+                                  >
+                                    -
+                                  </Button>
+                                  <span className="min-w-[2rem] text-center">
+                                    {selectedItems[item.id]}
+                                  </span>
+                                  <Button
+                                    variant="outline"
+                                    size="icon"
+                                    onClick={() =>
+                                      adjustItemQuantity(item.id, 1)
+                                    }
+                                  >
+                                    +
+                                  </Button>
+                                </div>
+                              )}
+                            </div>
                           </div>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  ))}
-                </div>
-              )
-            )}
+                        </CardContent>
+                      </Card>
+                    ))}
+                  </div>
+                )
+              )}
           </main>
         )}
 
