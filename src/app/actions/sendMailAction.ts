3fa01e83675b03b97b7056f8933782ba33809dad
Randika Dilshan <rdilshan077788@gmail.com>
2025-03-11 08:47:46 +0530

    create the otp functions

---
+++ src/app/actions/sendMailAction.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import { getServiceRoleClient } from "@/db";
+import { createOtp } from "@/db/otp";
+import { sendRegistationEmail } from "@/services/brevo";
+import { generateOtp } from "@/utils/generateotp";
+import jwt from "jsonwebtoken";
+
+export async function sendMailAction(
+  email: string,
+  placeName: string,
+  inviteCode: string
+) {
+  try {
+    const client = getServiceRoleClient();
+    const otp = await generateOtp(6);
+    const link = await createLinkAction(otp, inviteCode, email);
+    await sendRegistationEmail(email, link, placeName);
+    await createOtp(client, email, otp.toString());
+    return true;
+  } catch (error) {
+    return error;
+  }
+}
+
+export async function createLinkAction(
+  Otp: number,
+  inviteCode: string,
+  email: string
+) {
+  const secretKey = process.env.JWT_SECRET as string;
+
+  if (!secretKey) {
+    throw new Error("JWT_SECRET is not defined in environment variables.");
+  }
+  // Create JWT token with email & OTP
+  const token = jwt.sign({ email, otp: Otp }, secretKey, { expiresIn: "10m" });
+
+  const link =
+    process.env.NEXT_PUBLIC_BASE_URL +
+    "/onboarding?invite_code=" +
+    inviteCode +
+    "&otpToken=" +
+    token;
+  return link;
+}
