aaaa7b919a6047367ead98f8a893be52d16a6007
kev-techi <kevin@techi.be>
2025-03-27 11:02:18 +0100

    migration for fees

--- src/viva/transactions.ts
+++ src/viva/transactions.ts
@@ -1,4 +1,4 @@
-import { VivaTransaction } from ".";
+import { VivaTransaction, VivaLegacyTransactionsResponse } from ".";
 
 export const getVivaTransaction = async (
   transactionId: string
@@ -44,4 +44,37 @@ const getVivaAccessToken = async () => {
   const data = await response.json();
 
   return data.access_token;
+};
+
+export const getFullTransaction = async (transactionId: string) => {
+  const basicAuth = Buffer.from(
+    `${process.env.VIVA_MERCHANT_ID}:${process.env.VIVA_API_KEY}`
+  ).toString("base64");
+
+  const response = await fetch(
+    `https://www.vivapayments.com/api/transactions/${transactionId}`,
+    {
+      headers: {
+        Authorization: `Basic ${basicAuth}`,
+      },
+    }
+  );
+
+  if (response.status !== 200) {
+    console.error("Error getting transaction", response.statusText);
+    return null;
+  }
+
+  const data: VivaLegacyTransactionsResponse = await response.json();
+  if (!data.Success) {
+    console.error("Transaction not successful", data.ErrorText);
+    return null;
+  }
+
+  if (data.Transactions.length === 0) {
+    console.error("Transaction not found", transactionId);
+    return null;
+  }
+
+  return data.Transactions[0];
 };
